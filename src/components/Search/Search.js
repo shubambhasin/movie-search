01081bb258fd40d3ca19bdaf17a9f9c7d2c2b342
Shubam Bhasin <33823641+shubambhasin@users.noreply.github.com>
2021-01-26 12:46:28 +0530

    feat: fetching added

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,8 +1,20 @@
 import React, { useState } from "react";
 import "./Search.css";
 const Search = () => {
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState(21);
 
+  const apiKey = "5ab85caa7b79798db1108c5cc409d303";
+  function searchHandle(e) {
+    fetch(
+      `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${searchTerm}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data.results);
+      });
+
+    e.preventDefault();
+  }
   return (
     <div className="search">
       <nav>
@@ -11,9 +23,12 @@ const Search = () => {
         <a href="/">Trending</a>
         <a href="/">Top 20</a>
       </nav>
-      <form type="submit" className="search-div">
-        <input placeholder="Enter a movie name..." />
-        <button>Search</button>
+      <form onSubmit={searchHandle} className="search-div">
+        <input
+          placeholder="Enter a movie name..."
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button onClick={searchHandle}>Search</button>
       </form>
     </div>
   );
